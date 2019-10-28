c4ee3a3fbe920bf755df71f4eaef1cc2ad40fa6d
Meowmere <miaumere@gmail.com>
2019-10-28 11:00:24 +0100

    changed request mapping to getMapping

--- main/src/main/java/com/meowmere/main/Controllers/CharactersListController.java
+++ main/src/main/java/com/meowmere/main/Controllers/CharactersListController.java
@@ -3,8 +3,8 @@ import com.meowmere.main.DTO.CharacterDTO;
 import com.meowmere.main.DTO.CharactersMenuDTO;
 import com.meowmere.main.services.CharactersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,12 +16,12 @@ public class CharactersListController {
     @Autowired
     CharactersService charactersService;
 
-    @RequestMapping("/get-characters")
+    @GetMapping("/get-characters")
     public List<CharactersMenuDTO> CharListBean(){
         return charactersService.findCharList();
     }
 
-    @RequestMapping("/get-character/{characterId}")
+    @GetMapping("/get-character/{characterId}")
     public CharacterDTO CharacterBean(@PathVariable Long characterId) {
         return charactersService.findByExternalId(characterId);
     }
--- main/src/main/java/com/meowmere/main/Controllers/SideCharactersController.java
+++ main/src/main/java/com/meowmere/main/Controllers/SideCharactersController.java
@@ -3,7 +3,7 @@ package com.meowmere.main.Controllers;
 import com.meowmere.main.DTO.SideCharacterDTO;
 import com.meowmere.main.services.SideCharactersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class SideCharactersController {
     @Autowired
     SideCharactersService sideCharactersService;
 
-    @RequestMapping("/side-characters")
+    @GetMapping("/side-characters")
     public List<SideCharacterDTO> SideCharacterBean() {
         return sideCharactersService.findAllSideCharacters();
     }
