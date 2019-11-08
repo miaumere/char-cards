2ada937ede7087bb55f5652df061b71197857b2b
Meowmere <miaumere@gmail.com>
2019-11-08 21:11:15 +0100

    added icon component

---
+++ main/frontend/alea/src/app/enum/icon.enum.ts
@@ -0,0 +1,4 @@
+export enum IconEnum {
+  Warning = 'warning',
+  NewCharacter = 'new-character'
+}
--- main/frontend/alea/src/app/shared/components/icon/icon.component.ts
+++ main/frontend/alea/src/app/shared/components/icon/icon.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IconEnum } from 'src/app/enum/icon.enum';
 
 @Component({
   selector: 'app-icon',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconComponent implements OnInit {
 
+  @Input() icon = IconEnum;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.icon)
   }
 
 }
