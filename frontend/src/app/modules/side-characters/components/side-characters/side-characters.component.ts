7eb6a918ba1c838bde3d72e46e5aec5144231855
k.sliz <miaumere@gmail.com>
2020-03-17 14:03:48 +0100

    added small changes, styled search bar

--- frontend/src/app/modules/side-characters/components/side-characters/side-characters.component.ts
+++ frontend/src/app/modules/side-characters/components/side-characters/side-characters.component.ts
@@ -1,8 +1,12 @@
+import { CharacterForListItem } from './../../../characters/models/character-for-list-item.model';
+import { IBook } from './../../../admin-panel/models/book.model';
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/core/base.component';
 import { SideCharactersService } from 'src/app/core/service/side-characters.service';
-import { SideCharacter } from 'src/app/modules/side-characters/models/side-characters.model';
-import { finalize } from 'rxjs/operators';
+import { finalize, filter } from 'rxjs/operators';
+import { SideCharacter } from '../../models/side-character.model';
+import { CharactersService } from 'src/app/core/service/characters.service';
+import { ICharacterItem } from 'src/app/modules/characters/models/character-item.model';
 
 @Component({
   selector: 'app-side-characters',
@@ -11,27 +15,59 @@ import { finalize } from 'rxjs/operators';
 })
 export class SideCharactersComponent extends BaseComponent implements OnInit {
   sideCharacters: SideCharacter[];
+  books: IBook[] | null = null;
+  characters: ICharacterItem[]
   loading = true;
 
-  constructor(private _sideCharactersService: SideCharactersService) {
+  constructor(
+    private _sideCharactersService: SideCharactersService,
+    private _charactersService: CharactersService
+  ) {
     super();
   }
 
   ngOnInit() {
     this.getSideCharacters();
+    this.getBooks();
+    this.getCharacterList();
   }
 
   getSideCharacters() {
-    this._sideCharactersService
-      .getSideCharacters()
-      .pipe(
-        finalize(() => {
-          this.loading = false;
+    this.subscriptions$.add(
+      this._sideCharactersService
+        .getSideCharacters()
+        .pipe(
+          finalize(() => {
+            this.loading = false;
+          })
+        ).subscribe(sideCharacters => {
+          this.sideCharacters = sideCharacters;
         })
-      ).subscribe(sideCharacters => {
-        this.sideCharacters = sideCharacters;
-        this.loading = false;
-      });
+    )
+  }
+
+  getBooks() {
+    this.subscriptions$.add(
+      this._sideCharactersService
+        .getBooks()
+        .pipe(
+          finalize(() => {
+            this.loading = false;
+          })
+        ).subscribe(books => {
+          this.books = books;
+        })
+    )
+  }
+
+  getCharacterList() {
+    this.subscriptions$.add(
+      this._charactersService.getCharacters()
+        .subscribe(characters => {
+          this.characters = characters;
+        })
+    )
+
   }
 
 }
