38aaa27a32ce83b3ee9a71e448ece7a529d8dd3d
k.sliz <kinga.pawliszyn@gmail.com>
2020-06-24 15:38:08 +0200

    preferences chart - added zoom effect (WIP)

--- frontend/src/app/modules/characters/components/characters-article/components/character-card/current-preferences-chart/current-preferences-chart.component.ts
+++ frontend/src/app/modules/characters/components/characters-article/components/character-card/current-preferences-chart/current-preferences-chart.component.ts
@@ -13,6 +13,8 @@ export class CurrentPreferencesComponent extends BaseComponent implements OnInit
   @ViewChild('preferencesChart')
   private chartContainer: ElementRef;
 
+  svgViewport;
+
   @Input() preferences: CharacterPreferences[];
   constructor(
     private _translate: TranslateService
@@ -33,68 +35,40 @@ export class CurrentPreferencesComponent extends BaseComponent implements OnInit
     const margin = { top: 30, right: 40, bottom: 50, left: 60 };
     const height = svgHeight - margin.top - margin.bottom;
     const width = svgWidth - margin.left - margin.right;
-    let circles: any = [];
+    let circles;
 
+    const xAxisScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, width]);
 
     const svgViewport = d3.select(element)
       .append('svg')
       .attr('width', svgWidth)
       .attr('height', svgHeight)
-      .style('background', 'rgba(245, 245, 245, 0.3)');
+      .style('background', 'rgba(245, 245, 245, 0.3)')
+      .call(d3.zoom().on('zoom', () => {
+        console.log('zium');
+
+        const new_xScale = d3.event.transform.rescaleX(xAxisScale);
 
 
-    const data = [0, 100];
-    const extent = d3.extent(data);
-
-    const xAxisScale = d3.scaleLinear()
-      .domain(extent as any)
-      .range([0, width]);
-
-    // const yAxisScale = d3.scaleLinear()
-    //   .domain([0, 5])
-    // .range([height, 0]);
+        gX.transition()
+          .duration(50)
+          .call(xAxis.scale(new_xScale));
 
 
-    // create axis objects
-    const xAxis = d3.axisBottom(xAxisScale);
-
-    const zoom = d3.zoom()
-      .scaleExtent([1, 10])
-      .on('zoom', () => {
-        const transform: d3.ZoomTransform = d3.event.transform;
-        // const transformedXScale = transform.rescaleX<any>(xAxisScale)
-
-        transform.scale(1);
-
-        const new_xScale = transform.rescaleX(xAxisScale);
-        // update axes
-        gX.call(xAxis.scale(new_xScale));
-
-        // lines.attr('cx', function(d) { return transform.applyX(xAxisScale(d)); });
-        // bars.attr('cx', function(d) { return transform.applyX(xAxisScale(d)); });
-
-
-
-        for (const circle of circles) {
-          console.log(transform)
-          // const transformString = `translate(${transform.x},0) scale(${transform.k},1)`
-          const transformString = `translate(${transform.x},0) scale(${transform.k},1)`
-
-          circle.attr('transform', transformString);
-          // circle.style('transform', 'scale(1)')
-
+        if (circles) {
+          circles.attr('cx', (d) => { return new_xScale(d.range); });
         }
 
 
-        // update circle
-      });
+      }));
+
 
     // Inner Drawing Space
     const innerSpace = svgViewport.append('g')
       .attr('class', 'inner_space')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-      .call(zoom);
-
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     for (const pref of this.preferences) {
       const defs = svgViewport.append('defs');
@@ -113,40 +87,68 @@ export class CurrentPreferencesComponent extends BaseComponent implements OnInit
         .attr('width', '1')
         .attr('preserveAspectRatio', 'xMidYMid slice')
         .attr('xlink:href', `data:image/${pref.relCharAvatar?.extension};base64,${pref.relCharAvatar?.image}`);
+
+
     }
 
+    circles = innerSpace.append('g')
+      .attr('id', 'circles')
+      .selectAll('circle')
+      .data(this.preferences)
+      .enter()
+      .append('circle')
+      .attr('r', 19)
+      .attr('fill', (d) => { console.log(d); return 'red' })
+      .attr('cy', '0')
+      .attr('cx', (d) => { return xAxisScale(d.range); })
+      .attr('stroke', 'black')
+      .attr('fill', (d) => {
+        return `url(#image_${d.relCharId})`
+      })
+    // .attr('fill', 'url(#image_' + pref.relCharId + ')');
+    // .style('fill', 'red');
 
-    // Draw Axis
+
+
+    // create axis objects
+    const xAxis = d3.axisBottom(xAxisScale);
     const gX = innerSpace.append('g')
       .attr('transform', 'translate(0,' + height + ')')
       .call(xAxis);
 
+
+
+
+
+    // Draw Axis
     // const gY = innerSpace.append('g')
     //   .attr('class', 'axis axis--y')
     //   .call(yAxis);
 
     // append zoom area
-    const view = innerSpace.append('rect')
-      .attr('class', 'zoom')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('fill', 'transparent')
-      .call(zoom);
+    // const view = innerSpace.append('rect')
+    //   .attr('class', 'zoom')
+    //   .attr('width', width)
+    //   .attr('height', height)
+    //   .attr('fill', 'transparent')
+    //   .call(zoom);
 
 
-    for (const pref of this.preferences) {
+    // for (const pref of this.preferences) {
 
-      const newCircle = innerSpace.append('circle')
-        .attr('cx', xAxisScale(pref.range))
-        .attr('r', 20)
-        .attr('stroke', 'black')
-        .attr('fill', 'url(#image_' + pref.relCharId + ')')
+    //   const newCircle = innerSpace.append('circle')
+    //     .attr('cx', xAxisScale(pref.range))
+    //     .attr('class', 'circle')
+    //     .attr('r', 20)
+    //     .attr('stroke', 'black')
+    //     .attr('fill', 'url(#image_' + pref.relCharId + ')');
 
-      circles.push(newCircle)
+    //   circles.push(newCircle);
 
 
-    }
+    // }
 
 
   }
+
 }
