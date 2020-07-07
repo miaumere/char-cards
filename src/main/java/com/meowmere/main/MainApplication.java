477efef7ff180b45ea9eea51c8756c8f969f4a66
k.sliz <kinga.pawliszyn@gmail.com>
2020-07-07 14:45:03 +0200

    AIE-74 Ustawienia - w zewnętrznym pliku

--- src/main/java/com/meowmere/main/MainApplication.java
+++ src/main/java/com/meowmere/main/MainApplication.java
@@ -5,6 +5,7 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.*;
@@ -13,8 +14,14 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("file:${app.home}/application.properties")
 public class MainApplication extends SpringBootServletInitializer {
+
 	public static void main(String[] args){
+	String logsPath = UtilsShared.GetMainDir();
+		System.setProperty("app.home", logsPath);
+
 		SpringApplication.run(MainApplication.class, args);
 	}
+
 }
