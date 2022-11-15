44087f118479fa948a0b41bb8b803d6bc07a21b9
miaumere <40804624+miaumere@users.noreply.github.com>
2022-11-15 19:18:42 +0100

    changed quotes display

--- src/main/resources/db/migration/V019__Add_new_columns_to_stories_entities.sql
+++ src/main/resources/db/migration/V019__Add_new_columns_to_stories_entities.sql
@@ -1,10 +1,10 @@
-ALTER TABLE public.book
-ADD symbol varchar(255);
-
-ALTER TABLE public.chapter
-  ADD action_place varchar(255),
-  ADD action_time varchar(255),
-  ADD  create_date int8,
-  ADD  visible bool;
-
-DROP TABLE public.relationship;
+--ALTER TABLE public.book
+--ADD symbol varchar(255);
+--
+--ALTER TABLE public.chapter
+--  ADD action_place varchar(255),
+--  ADD action_time varchar(255),
+--  ADD  create_date int8,
+--  ADD  visible bool;
+--
+--DROP TABLE public.relationship;
